7d77b05c06fe7f6d2a6f808839bc0ee321b4c989
Pavel Cherepanov <cherepanov.pavel@list.ru>
2023-01-08 15:30:59 +0500

    modified:   element-search/slider/task.js

--- element-search/slider/task.js
+++ element-search/slider/task.js
@@ -1,33 +1,25 @@
 const sliderItems = [...document.querySelectorAll('.slider__item')];
-const nextButton = document.querySelector('.slider__arrow_prev');
-const prevButton = document.querySelector('.slider__arrow_next');
-let currentIndex = 0;
+const nextButton = document.querySelector('.slider__arrow_next');
+const prevButton = document.querySelector('.slider__arrow_prev');
+let currentIndex = sliderItems.findIndex(index => index.classList.value.includes('slider__item_active'));
 
 
 function next(){
   const current = currentIndex;
-  if (currentIndex === sliderItems.length -1) {
-    currentIndex = 0;
-  } else {
-    currentIndex +=1;
-  }
+  currentIndex = currentIndex === sliderItems.length - 1 ? 0 : currentIndex + 1;
   changeSlide(current, currentIndex);
 };
 
 function prev(){
   const current = currentIndex;
-  if (currentIndex === 0) {
-    currentIndex = sliderItems.length -1;
-  } else {
-    currentIndex -=1;
-  }
+  currentIndex = currentIndex === 0 ? sliderItems.length - 1 : currentIndex + 1;
   changeSlide(current, currentIndex);
 };
 
 function changeSlide(current, active){
   sliderItems[current].classList.remove('slider__item_active');
   sliderItems[active].classList.add('slider__item_active');
-}
+};
 
 nextButton.onclick = next;
 prevButton.onclick = prev;
