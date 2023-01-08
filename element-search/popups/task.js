64e4bf6298744e63ebda69ea362300558de19988
Pavel Cherepanov <cherepanov.pavel@list.ru>
2023-01-08 16:29:43 +0500

    modified:   element-search/popups/task.html 	modified:   element-search/popups/task.js

--- element-search/popups/task.js
+++ element-search/popups/task.js
@@ -2,19 +2,23 @@ let close = [...document.querySelectorAll('.modal__close')];
 let show = document.querySelector('.show-success');
 let modalMain = document.getElementById('modal_main')
 let modalSuccess = document.getElementById('modal_success');
+let modal = document.querySelector('.modal');
 
-close[0].onclick = function(){
-  modalMain.remove('modal_active');
-};
+window.onload = function(){
+  modal.className = 'modal modal_active'
+}
+
+for (let i = 0; i < close.length; i++){
+  close[i].onclick = function(){
+    close[i].closest('.modal').remove('modal_active');
+  }
+}
 
 show.onclick = function(){
   modalMain.remove('modal_active');
   modalSuccess.className = 'modal modal_active';
 };
 
-close[2].onclick = function(){
-  modalSuccess.remove('modal_active');
-};
 
 
 
